4e08c1f379e49a4a5584b6961e73fca52489b7c4
sashathelambo <166897058+sashathelambo@users.noreply.github.com>
2024-08-27 19:43:11 -0500

    Update TestFactorizer.java

--- src/test/java/com/csc/TestFactorizer.java
+++ src/test/java/com/csc/TestFactorizer.java
@@ -1,6 +1,6 @@
 package com.csc;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,20 +14,22 @@ public class TestFactorizer {
     factorizer = new Factorizer();
   }
 
-  // Replace the following example tests with your own tests
-  // of the prime factorization function.
   @Test
-  public void testZero() {
-    assertEquals(2, factorizer.exampleMethod(0));
-  }
+    public void testZero() {
+        // Testing primeFactors with input 0, expecting null
+        assertNull(factorizer.primeFactors(0), "Prime factors of 0 should be null.");
+    }
 
-  @Test
-  public void testOne() {
-    assertEquals(2, factorizer.exampleMethod(1));
-  }
+    @Test
+    public void testOne() {
+        // Testing primeFactors with input 1, expecting null
+        assertNull(factorizer.primeFactors(1), "Prime factors of 1 should be null.");
+    }
 
-  @Test
-  public void testTen() {
-    assertEquals(1, factorizer.exampleMethod(10));
-  }
+    @Test
+    public void testTen() {
+        // Testing primeFactors with input 10, expecting [2, 5]
+        int[] expected = {2, 5};
+        assertArrayEquals(expected, factorizer.primeFactors(10), "Prime factors of 10 should be [2, 5].");
+    }
 }
