d76780b8c48c2cf196a86209deaa946e03334ae0
sashathelambo <166897058+sashathelambo@users.noreply.github.com>
2024-08-27 19:47:06 -0500

    Update TestFactorizer.java

--- src/test/java/com/csc/TestFactorizer.java
+++ src/test/java/com/csc/TestFactorizer.java
@@ -1,6 +1,7 @@
 package com.csc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
