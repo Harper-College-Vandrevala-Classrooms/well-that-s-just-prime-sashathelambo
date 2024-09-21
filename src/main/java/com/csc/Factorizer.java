65cbe85d6195fa505998a7f5b427cf04d7e6abc3
sashathelambo <166897058+sashathelambo@users.noreply.github.com>
2024-09-21 14:42:52 -0500

    Update Factorizer.java

--- src/main/java/com/csc/Factorizer.java
+++ src/main/java/com/csc/Factorizer.java
@@ -1,31 +1,64 @@
 package com.csc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Factorizer {
 
-    public int[] primeFactors(int number) {
+    public List<Integer> primeFactors(int number) {
         if (number <= 1) {
             return null;
         }
 
-        int[] factors = new int[100];
-        int index = 0;
+        List<Integer> factors = new ArrayList<>();
 
         int i = 2;
         while (number > 1) {
             while (number % i == 0) {
-                factors[index] = i;
-                index++;
+                factors.add(i);
                 number = number / i;
             }
             i++;
         }
 
-        int[] result = new int[index];
-        for (int j = 0; j < index; j++) {
-            result[j] = factors[j];
+        return factors;
+    }
+
+    public boolean isPrime(int number) {
+        List<Integer> factors = primeFactors(number);
+        return factors != null && factors.size() == 1 && factors.get(0) == number;
+    }
+
+    public boolean isComposite(int number) {
+        return number > 1 && !isPrime(number);
+    }
+
+    public String reduce(int numerator, int denominator) {
+        List<Integer> numFactors = primeFactors(numerator);
+        List<Integer> denFactors = primeFactors(denominator);
+
+        if (numFactors == null || denFactors == null) {
+            return numerator + "/" + denominator;
         }
 
-        return result;
+        for (int factor : numFactors) {
+            if (denFactors.contains(factor)) {
+                numFactors.remove((Integer) factor);
+                denFactors.remove((Integer) factor);
+            }
+        }
+
+        int reducedNumerator = 1;
+        for (int factor : numFactors) {
+            reducedNumerator *= factor;
+        }
+
+        int reducedDenominator = 1;
+        for (int factor : denFactors) {
+            reducedDenominator *= factor;
+        }
+
+        return reducedNumerator + "/" + reducedDenominator;
     }
 
     public static void main(String[] args) {
@@ -36,9 +69,24 @@ public class Factorizer {
         printFactors(1, factorizer.primeFactors(1));
         printFactors(100, factorizer.primeFactors(100));
         printFactors(210, factorizer.primeFactors(210));
+
+        System.out.println("Is 28 prime? " + factorizer.isPrime(28));
+        System.out.println("Is 13 prime? " + factorizer.isPrime(13));
+        System.out.println("Is 1 prime? " + factorizer.isPrime(1));
+        System.out.println("Is 100 prime? " + factorizer.isPrime(100));
+        System.out.println("Is 210 prime? " + factorizer.isPrime(210));
+
+        System.out.println("Is 28 composite? " + factorizer.isComposite(28));
+        System.out.println("Is 13 composite? " + factorizer.isComposite(13));
+        System.out.println("Is 1 composite? " + factorizer.isComposite(1));
+        System.out.println("Is 100 composite? " + factorizer.isComposite(100));
+        System.out.println("Is 210 composite? " + factorizer.isComposite(210));
+
+        System.out.println("Reduced fraction of 12/26: " + factorizer.reduce(12, 26));
+        System.out.println("Reduced fraction of 100/210: " + factorizer.reduce(100, 210));
     }
 
-    private static void printFactors(int number, int[] factors) {
+    private static void printFactors(int number, List<Integer> factors) {
         if (factors == null) {
             System.out.println("Prime factors of " + number + ": null");
         } else {
